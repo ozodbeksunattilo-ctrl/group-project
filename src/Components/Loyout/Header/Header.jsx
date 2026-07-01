d3a0843e72a0fedac15814c5a6d165c3aa730990
ozodbeksunattilo <ozodbeksunattilo@gmail.com>
2026-07-01 09:37:17 +0500

    Header update

--- src/Components/Loyout/Header/Header.jsx
+++ src/Components/Loyout/Header/Header.jsx
@@ -3,9 +3,9 @@ function Header() {
   return (
     <header className="bg-slate-950 text-white border-b border-slate-800 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-6 py-5 flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-cyan-400 cursor-pointer">
-          BE Project
-        </h1>
+      <h1 className="text-3xl font-bold text-cyan-400">
+  BE Project 🚀
+</h1>
 
         <ul className="hidden md:flex gap-8 font-medium">
           <li className="hover:text-cyan-400 duration-300 cursor-pointer">
